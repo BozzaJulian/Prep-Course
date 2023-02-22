423b2606f030a1ed5ec0f30e51a3f2c89c6b8c18
BozzaJulian <bozzajulian7@gmail.com>
2023-02-22 11:18:42 -0300

    AgregarPropiedad

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -18,7 +18,9 @@ function agregarPropiedad (objeto, property) {
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
-  // Tu código:
+  // Tu código:  
+  objeto[property]=null;
+  return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {
