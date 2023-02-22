48e3abf94718ab2987ed1e4ebfdd394f9d482e80
BozzaJulian <bozzajulian7@gmail.com>
2023-02-22 11:38:50 -0300

    invocarmetodo

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -28,6 +28,7 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
