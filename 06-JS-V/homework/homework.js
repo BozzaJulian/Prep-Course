62d1c4d40350b8a3cf7dc2dbd44a7b1630f35881
BozzaJulian <bozzajulian7@gmail.com>
2023-02-27 17:23:53 -0300

    agregarMetodoPrototype

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,28 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-}
+  
+  class Usuario {
+    Constructor(usuario, nombre, email, password){
+    this.usuario = usuario;
+    this.nombre = nombre;
+    this.email = email;
+    this.password = password;
+    } 
+  } 
+    Usuario.prototype.saludar = function() {
+      return "Hola,mi nombre es " + this.nombre;
+  }
+  return Usuario;  
+} 
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function () {
+    return "Hello World!";
+  }
 }
 
 function agregarStringInvertida() {
