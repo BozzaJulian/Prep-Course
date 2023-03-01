875570df4556f0c42f566b16bc8d68b24028703a
BozzaJulian <bozzajulian7@gmail.com>
2023-03-01 10:53:41 -0300

    invoCallback,sumArray,forEach,map,filter

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -15,14 +15,14 @@ return popo;
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-return cb;
+return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
- return cb (n1,n2)
+return cb (n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -30,12 +30,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let suma = 0
+  for (i = 0; i < numeros.length; i++){
+    suma += numeros[i];    
+  } 
+  cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(valor => { 
+    cb(valor);    
+  });
 }
 
 function map(array, cb) {
@@ -43,12 +51,28 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  let total = 0;
+  let array1 = [];
+  array.map(valor=> {
+    total = cb(valor)
+    array1.push(total);  
+  });
+  return array1;  
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
-  //Tu código:
+  //Tu código
+  let palabra = [];
+  let word = 0;
+  for (let i= 0; i < array.length; i++) {
+    word = array[i].split('');
+    if (word[0] === 'a'){
+      palabra.push(array[i])
+    }    
+  }
+  return palabra;
 }
 
 // No modificar nada debajo de esta línea
