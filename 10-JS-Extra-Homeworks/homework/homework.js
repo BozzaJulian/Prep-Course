d5302df60838020c0db3b28e06d246c5008bf2d6
BozzaJulian <bozzajulian7@gmail.com>
2023-03-06 15:02:08 -0300

    capicua

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -81,6 +81,15 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let split = numero.toString();
+  split = split.split('');
+  split.reverse();
+  split= split.join('');
+  if (numero == split){
+    return "Es capicua";
+  }else {
+    return "No es capicua";
+  }
 }
 
 
