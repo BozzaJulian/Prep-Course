a1c783cb9dee73fa9f3d1f06563c8cc36df4e0a8
BozzaJulian <bozzajulian7@gmail.com>
2023-03-08 13:44:49 -0300

    sortArray,buscoIterseccion

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -112,6 +112,10 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  let result = arr.sort( function (a,b){
+    return a.length - b.length;
+  });
+  return result;
 }
 
 
@@ -121,6 +125,15 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  let array = []
+  for (let i= 0; i< arreglo1.length; i++) {
+    for (let j = 0; j < arreglo2.length; j++) {
+      if (arreglo1[i] === arreglo2[j]){
+        array.push(arreglo1[i]);
+      } 
+    }
+  }
+  return array;
 }
 
 
